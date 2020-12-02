502103d4b9094d3f1d926b0550f4729d77225bfb
Alexander Knapp <alexander.knapp@objectbay.com>
2020-12-02 13:18:51 +0100

    initial commit

---
+++ src/main/kotlin/aoc1/main.kt
@@ -0,0 +1,51 @@
+package aoc1
+
+import java.io.File
+
+fun main() {
+    println("Hello World!")
+    val dataList = readFileAsLinesUsingUseLines("src/main/resources/dataInput_1.txt")
+    val numberList = mutableListOf<Int>()
+    dataList.forEach { numberList.add(it.toInt()) }
+    val resultPair = sumsToTarget(numberList.toIntArray(), 2020)
+    println(resultPair)
+    println(multiplyList(resultPair))
+    val resultTriplet = sums3ToTarget(numberList.toIntArray(), 2020)
+    println(resultTriplet)
+    println(multiplyList(resultTriplet))
+}
+
+fun readFileAsLinesUsingUseLines(fileName: String): List<String>
+        = File(fileName).useLines { it.toList() }
+
+fun equals2020(a: Int, b: Int): Boolean {
+    return a + b == 2020
+}
+
+fun multiplyList(arr: List<Int>): Int {
+    return arr.reduce { acc, i -> acc * i }
+}
+
+fun sumsToTarget(arr: IntArray, k: Int): List<Int> {
+    val values: HashSet<Int> = HashSet()
+    for (i in arr.indices) {
+        if (values.contains(k-arr[i])) return intArrayOf(arr[i], k-arr[i]).sortedArray().asList()
+        values.add(arr[i])
+    }
+    return intArrayOf().asList()
+}
+
+fun sums3ToTarget(arr: IntArray, k: Int): List<Int> {
+    val values: HashSet<Int> = HashSet()
+    for (i in arr.indices) {
+        for (j in i until arr.size) {
+            if (values.contains(k-arr[i]-arr[j]) && arrayListOf(arr[i], k - arr[i] - arr[j], arr[j]).toSet().size == 3) {
+                return intArrayOf(arr[i], k - arr[i] - arr[j], arr[j]).sortedArray().asList()
+            }
+            values.add(arr[j])
+        }
+        values.add(arr[i])
+    }
+    return intArrayOf().asList()
+}
+
---
+++ src/test/kotlin/aoc1/Aoc1test.kt
@@ -0,0 +1,31 @@
+package aoc1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class aoc1test {
+    @Test
+    fun twoNumbersShouldEqual2020() {
+        assertEquals(equals2020(2010, 10), true)
+    }
+
+    @Test
+    fun sumOfTwoExistsTest() {
+        assertEquals(sumsToTarget(intArrayOf(1,3,7), 8), intArrayOf(1,7).asList())
+        assertEquals(sumsToTarget(intArrayOf(1,3,7), 9), intArrayOf().asList())
+    }
+
+    @Test
+    fun multiplyListTest() {
+        assertEquals(multiplyList(listOf(1,2,3,4)), 24)
+    }
+
+    @Test
+    fun sumOfThreeExistsTest() {
+        assertEquals(sums3ToTarget(intArrayOf(1,3,4,7),8), intArrayOf(1,3,4).asList())
+        assertEquals(sums3ToTarget(intArrayOf(1,5,7,13,20),13), intArrayOf(1,5,7).asList())
+        assertEquals(sums3ToTarget(intArrayOf(1,3,4,5,6),15), intArrayOf(4,5,6).asList())
+        assertEquals(sums3ToTarget(intArrayOf(1,3,4,8),8), intArrayOf(1,3,4).asList())
+        assertEquals(sums3ToTarget(intArrayOf(1,2,4,8),8), intArrayOf().asList())
+    }
+}
